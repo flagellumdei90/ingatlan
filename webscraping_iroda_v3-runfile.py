5bacdca515581e5675031d09d4c8bf6659218c46
Gulyati <gulyati@gmail.com>
2025-04-24 13:27:28 +0200

    Update webscraping_iroda_v3-runfile.py

--- webscraping_iroda_v3-runfile.py
+++ webscraping_iroda_v3-runfile.py
@@ -29,7 +29,7 @@ driver.get(url)
 wait = WebDriverWait(driver, 20)
 
 #Scroll down to the bottom of the page
-for x in range(350):
+for x in range(10):
     driver.execute_script("window.scrollBy(0, 250);")
     time.sleep(10)  # Give time for new items to load
 
